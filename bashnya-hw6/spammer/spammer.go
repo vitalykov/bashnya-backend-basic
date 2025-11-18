b78d1d938e46d780163a2dde90eb98c44ad54c5e
Vitaly Kovalev <kovalev.vitaly.msu@yandex.ru>
2025-11-18 16:58:52 +0300

    [bashnya-hw6] spammer done in 102 lines

--- bashnya-hw6/spammer/spammer.go
+++ bashnya-hw6/spammer/spammer.go
@@ -1,25 +1,102 @@
 package main
 
-func RunPipeline(cmds ...cmd) {
+import (
+	"fmt"
+	"slices"
+	"sync"
+)
 
+func RunPipeline(cmds ...cmd) {
+	chans := make([]chan any, 0, len(cmds)+1)
+	for range cap(chans) {
+		chans = append(chans, make(chan any))
+	}
+	var wg sync.WaitGroup
+	for i, c := range cmds {
+		wg.Go(func() {
+			defer close(chans[i+1])
+			c(chans[i], chans[i+1])
+		})
+	}
+	wg.Wait()
 }
 
 func SelectUsers(in, out chan interface{}) {
-	// 	in - string
-	// 	out - User
+	var emails sync.Map
+	var wg sync.WaitGroup
+	for email := range in {
+		wg.Go(func() {
+			u := GetUser(email.(string))
+			if _, ok := emails.LoadOrStore(u.Email, struct{}{}); !ok {
+				out <- u
+			}
+		})
+	}
+	wg.Wait()
+}
+
+func findMessages(wg *sync.WaitGroup, out chan any, users []User) {
+	wg.Go(func() {
+		msgs, _ := GetMessages(users...)
+		for _, m := range msgs {
+			out <- m
+		}
+	})
 }
 
 func SelectMessages(in, out chan interface{}) {
-	// 	in - User
-	// 	out - MsgID
+	batch := make([]User, GetMessagesMaxUsersBatch)
+	i := 0
+	var wg sync.WaitGroup
+	for u := range in {
+		batch[i] = u.(User)
+		i = (i + 1) % GetMessagesMaxUsersBatch
+		if i == 0 {
+			batchCopy := make([]User, GetMessagesMaxUsersBatch)
+			copy(batchCopy, batch)
+			findMessages(&wg, out, batchCopy)
+		}
+	}
+	if i > 0 {
+		batchCopy := make([]User, GetMessagesMaxUsersBatch)
+		copy(batchCopy, batch)
+		findMessages(&wg, out, batchCopy[:i])
+	}
+	wg.Wait()
 }
 
 func CheckSpam(in, out chan interface{}) {
-	// in - MsgID
-	// out - MsgData
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, HasSpamMaxAsyncRequests)
+	for mId := range in {
+		sem <- struct{}{}
+		wg.Go(func() {
+			defer func() { <-sem }()
+			spam, _ := HasSpam(mId.(MsgID))
+			out <- MsgData{ID: mId.(MsgID), HasSpam: spam}
+		})
+	}
+	wg.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
-	// in - MsgData
-	// out - string
+	msgs := []MsgData{}
+	for mData := range in {
+		msgs = append(msgs, mData.(MsgData))
+	}
+	slices.SortFunc(msgs, func(a, b MsgData) int {
+		if a.HasSpam == b.HasSpam {
+			if a.ID < b.ID {
+				return -1
+			}
+			return 1
+		}
+		if a.HasSpam && !b.HasSpam {
+			return -1
+		}
+		return 1
+	})
+	for _, mData := range msgs {
+		out <- fmt.Sprintf("%v %d", mData.HasSpam, mData.ID)
+	}
 }
